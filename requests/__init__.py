3985fd8498972158f06eec3f04d8832870ffbdeb
Bart Broere <b.broere@nfi.nl>
2021-11-03 14:42:56 +0100

    Add a docstring to inform confused passers-by

--- requests/__init__.py
+++ requests/__init__.py
@@ -1,7 +1,14 @@
+"""
+Drop-in replacement for the requests module for Pyodide.
+Not intended for use with normal CPython, as it depends on the js proxy provided by pyodide.
+
+Don't expect this to be fully feature complete with the original Python requests module.
+It aims to cover the most common uses.
+"""
+import json
 from collections import Mapping
 
-from js import XMLHttpRequest, Blob, URLSearchParams
-import json
+from js import Blob, URLSearchParams, XMLHttpRequest
 
 
 class Response:
